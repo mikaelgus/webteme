cf89ce528dcebe7955be5a518174ee7b0b3f1a31
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-01-11 14:07:51 +0200

    tehtävän 3 ruokalista

--- src/index.js
+++ src/index.js
@@ -1,96 +1,87 @@
-console.log('Arvaa numero!');
 
-const minRandomValue = 1; //ramdom pienin arvo
-const maxRandomValue = 20; //random maksimi arvo
+const menu = document.querySelector('.menu');
 
-let randomNumber = Math.floor(Math.random() * (maxRandomValue - minRandomValue) + minRandomValue);
-
-const guesses = document.querySelector('.guesses');
-const counter = document.querySelector('.counter');
-const timeUsed = document.querySelector('.timeUsed');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
-
-let guessCount = 1;
-let resetButton;
-
-let startingTime = 0;
-let endingTime = 0;
-
-const checkGuess = () => {
-  console.log('lähtöaika: ', startingTime);
-  const maxNumberOfGuesses = 10; //arvauksien maksimimäärä
-  const userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
-    startingTime = Date.now();
+let coursesEn = ["Hamburger, cream sauce and poiled potates",
+                "Goan style fish curry and whole grain rice",
+                "Vegan Chili sin carne and whole grain rice",
+                "Broccoli puree soup, side salad with two napas",
+                "Lunch baguette with BBQ-turkey filling",
+                "Cheese / Chicken / Vege / Halloum burger and french fries"];
+let coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+                "Goalaista kalacurrya ja täysjyväriisiä",
+                "Vegaani Chili sin carne ja täysjyväriisi",
+                "Parsakeittoa, lisäkesalaatti kahdella napaksella",
+                "Lunch baguette with BBQ-turkey filling",
+                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+//tulostetaan suomenkielinen menu
+const printFinMenu = () => {
+    document.getElementById('menu').innerHTML = '';
+    coursesFi.forEach(meal => {
+      document.getElementById('menu').innerHTML +=
+      `<p>${meal}</p><p> * * * </p>`;
+    });
+};
+//tulostetaan englanninkilinen menu
+const printEngMenu = () => {
+  document.getElementById('menu').innerHTML = '';
+  coursesEn.forEach(meal => {
+    document.getElementById('menu').innerHTML +=
+    `<p>${meal}</p><p> * * * </p>`;
+  });
+};
+//kielen valinta
+let languageBool = true;
+const printMenu = () => {
+  languageBool = languageBool ? false : true;
+  if(languageBool === false){
+    printEngMenu();
   }
-  guesses.textContent += userGuess + ' ';
-  if (userGuess === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    endingTime = Date.now();
-    console.log('loppuaika: ', endingTime);
-    timerTime(endingTime - startingTime); //lähetetään millisekunnit funktioon
-    setGameOver();
-  } else if (guessCount === maxNumberOfGuesses) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    lowOrHi.textContent = '';
-    endingTime = Date.now();
-    console.log('loppuaika: ', endingTime);
-    timerTime(endingTime - startingTime); //lähetetään millisekunnit funktioon
-    setGameOver();
-  } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    if(userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if(userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
+  if(languageBool === true){
+    printFinMenu();
+  }
+};
+//menun sorttaus
+let sortBool = true;
+const sortMenu = () => {
+  document.getElementById('menu').innerHTML = '';
+  sortBool = sortBool ? false : true;
+  if(sortBool === false){
+    coursesEn.sort();
+    coursesFi.sort();
+    if(languageBool === true){
+      printFinMenu();
+    }
+    if(languageBool === false){
+      printEngMenu();
     }
   }
-  counter.textContent = 'Number of guesses: ' + guessCount; //tulostetaan arvauksien määrä
-  guessCount++;
-  guessField.value = '';
-  guessField.focus();
-};
-
-guessSubmit.addEventListener('click', checkGuess);
-
-const setGameOver = () => {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start New Game';
-  document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
-};
-//laske timeri ja tulosta se
-const timerTime = (timeValue) => {
-  const dateValue = new Date(timeValue);
-  timeUsed.textContent = 'Time used: ' + dateValue.toLocaleString("en-US", {second: "numeric"}) + ' seconds';
-};
-
-const resetGame = () => {
-  guessCount = 1;
-
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (const resetPara of resetParas) {
-    resetPara.textContent = '';
+  if(sortBool === true){
+    coursesEn.reverse();
+    coursesFi.reverse();
+    if(languageBool === true){
+      printFinMenu();
+    }
+    if(languageBool === false){
+      printEngMenu();
+    }
   }
-
-  resetButton.parentNode.removeChild(resetButton);
-
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
-
-  lastResult.style.backgroundColor = 'white';
-
-  randomNumber = Math.floor(Math.random() * 100) + 1;
 };
+//random sapuska
+const randomMeal = () => {
+  let food;
+  document.getElementById('menu').innerHTML = '';
+  if(languageBool === true){
+    food = coursesFi[Math.floor(Math.random() * coursesFi.length)];
+  }
+  if(languageBool === false){
+    food = coursesEn[Math.floor(Math.random() * coursesEn.length)];
+  }
+  document.getElementById('menu').innerHTML = '<br><p>RANDOM LOUNAS JUURI SINULLE!</p><p> * * * </p>'
+  + `<p> ${food} </p><p> * * * </p>`;
+};
+
+printFinMenu();
+document.getElementById('language').addEventListener('click', printMenu);
+document.getElementById('sort').addEventListener('click', sortMenu);
+document.getElementById('random').addEventListener('click', randomMeal);
+
--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     ]}),
     new HtmlWebpackPlugin({
-      title: 'Guessing Game',
+      title: 'Dummy Menu',
       meta: {
         viewport: 'width=device-width, initial-scale=1.0'
       },
