97ba6b2a55979f6d2691aa8c8bc8532c4b246a4c
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-01-16 14:43:24 +0200

    tehtävän 4 uuditettu lounalista

--- src/index.js
+++ src/index.js
@@ -1,105 +1,227 @@
-
-const menu = document.querySelector('.menu');
+const menu = document.querySelector(".menu1");
 
 //haetaan json
-import lunchMenu from './sodexo-day-example.json';
-console.log('lounasmenu jsonista', lunchMenu);
+import lunchMenu from "./sodexo-day-example.json";
+console.log("lounasmenu jsonista", lunchMenu);
 const finalMenu = lunchMenu.courses;
+
+//tehdään suomenkielinen ruokalista kaikkineen
+let coursesAll = [];
+const makeFinMenuAll = () => {
+  for (let i = 1; i < 10; i++) {
+    coursesAll.push(finalMenu[i]);
+  }
+  console.log("uusi fin menu kaikilla herkuilla", coursesAll);
+};
+makeFinMenuAll(finalMenu);
+
+//tulostetaan FIN menu kaikilla tiedoilla
+const tulostaFinMenu = () => {
+  document.getElementById("menu2").innerHTML = "";
+  document.getElementById("menu2").innerHTML = '<img id="sodexo" src="assets/ogosodexo.webp" alt="sodexon logo"/>' + '<h3>MENU 2</h3>';
+  coursesAll.forEach((meal) => {
+    document.getElementById("menu2").innerHTML += `<h3>${meal.category}</h3>
+    <p>${meal.title_fi}</p>
+    <p>${meal.properties}</p>
+    <p>${meal.price}</p>
+    <p></p>
+    <p> * * * </p>`;
+  });
+};
+
+//tulostetaan ENG menu kaikilla tiedoilla
+const tulostaEngMenu = () => {
+  document.getElementById("menu2").innerHTML = "";
+  document.getElementById("menu2").innerHTML = '<img id="sodexo" src="assets/ogosodexo.webp" alt="sodexon logo"/>' + '<h3>MENU 2</h3>';
+  coursesAll.forEach((meal) => {
+    document.getElementById("menu2").innerHTML += `<h3>${meal.category}</h3>
+    <p>${meal.title_en}</p>
+    <p>${meal.properties}</p>
+    <p>${meal.price}</p>
+    <p> * * * </p>`;
+  });
+};
 
 //tehdään suomenkielinen ruokalista
 let coursesFi = [];
 const makeFinMenu = () => {
-  for(let i = 1; i < 10; i++){
+  for (let i = 1; i < 10; i++) {
     coursesFi.push(finalMenu[i].title_fi);
-  };
-  console.log('uusi fin menu', coursesFi);
+  }
 };
 makeFinMenu(finalMenu);
 
 //tehdään englanninkielinen ruokalista
 let coursesEn = [];
 const makeEngMenu = () => {
-  for(let i = 1; i < 10; i++){
+  for (let i = 1; i < 10; i++) {
     coursesEn.push(finalMenu[i].title_en);
     //console.log(finalMenu[i].title_fi);
-  };
-  console.log('uusi eng menu', coursesEn);
+  }
 };
 makeEngMenu(finalMenu);
 
 //tulostetaan suomenkielinen menu
 const printFinMenu = () => {
-  document.getElementById('menu').innerHTML = '';
-  coursesFi.forEach(meal => {
-    document.getElementById('menu').innerHTML +=
-    `<p>${meal}</p><p> * * * </p>`;
+  document.getElementById("menu1").innerHTML = "";
+  document.getElementById("menu1").innerHTML = "<h3>MENU 1</h3>";
+  coursesFi.forEach((meal) => {
+    document.getElementById(
+      "menu1"
+    ).innerHTML += `<p>${meal}</p><p> * * * </p>`;
   });
 };
 //tulostetaan englanninkilinen menu
 const printEngMenu = () => {
-document.getElementById('menu').innerHTML = '';
-coursesEn.forEach(meal => {
-  document.getElementById('menu').innerHTML +=
-  `<p>${meal}</p><p> * * * </p>`;
-});
+  document.getElementById("menu1").innerHTML = "";
+  document.getElementById("menu1").innerHTML = "<h3>MENU 1</h3>";
+  coursesEn.forEach((meal) => {
+    document.getElementById(
+      "menu1"
+    ).innerHTML += `<p>${meal}</p><p> * * * </p>`;
+  });
 };
 
 //kielen valinta
 let languageBool = true;
 const printMenu = () => {
   languageBool = languageBool ? false : true;
-  if(languageBool === false){
+  if (languageBool === false) {
     printEngMenu();
+    tulostaEngMenu();
   }
-  if(languageBool === true){
+  if (languageBool === true) {
     printFinMenu();
+    tulostaFinMenu();
   }
 };
+//uusi shorttaus
+const shorttaaMenu = () => {
+  coursesAll.sort((a, b) => {
+    if(languageBool === true){
+      if(a.title_fi == b.title_fi){
+        return 0;
+      }
+      if(a.title_fi < b.title_fi){
+        return -1;
+      }
+      if(a.title_fi > b.title_fi){
+        return 1;
+      }
+    }
+    if(languageBool === false){
+      if(a.title_en == b.title_en){
+        return 0;
+      }
+      if(a.title_en < b.title_en){
+        return -1;
+      }
+      if(a.title_en > b.title_en){
+        return 1;
+      }
+    }
+  });
+};
+//uusi reverse
+const reverseMenu = () => {
+  coursesAll.sort((a, b) => {
+    if(languageBool === true){
+      if(a.title_fi == b.title_fi){
+        return 0;
+      }
+      if(a.title_fi < b.title_fi){
+        return 1;
+      }
+      if(a.title_fi > b.title_fi){
+        return -1;
+      }
+    }
+    if(languageBool === false){
+      if(a.title_en == b.title_en){
+        return 0;
+      }
+      if(a.title_en < b.title_en){
+        return 1;
+      }
+      if(a.title_en > b.title_en){
+        return -1;
+      }
+    }
+  });
+};
+
+
 
 //menun sorttaus
 let sortBool = true;
 const sortMenu = () => {
-  document.getElementById('menu').innerHTML = '';
+  document.getElementById("menu1").innerHTML = "";
   sortBool = sortBool ? false : true;
-  if(sortBool === false){
+  if (sortBool === false) {
     coursesEn.sort();
     coursesFi.sort();
-    if(languageBool === true){
+    shorttaaMenu();
+    if (languageBool === true) {
       printFinMenu();
+      tulostaFinMenu();
     }
-    if(languageBool === false){
+    if (languageBool === false) {
       printEngMenu();
+      tulostaEngMenu();
     }
   }
-  if(sortBool === true){
+  if (sortBool === true) {
     coursesEn.reverse();
     coursesFi.reverse();
-    if(languageBool === true){
+    reverseMenu();
+    if (languageBool === true) {
       printFinMenu();
+      tulostaFinMenu();
     }
-    if(languageBool === false){
+    if (languageBool === false) {
       printEngMenu();
+      tulostaEngMenu();
     }
   }
 };
 //random sapuska
 const randomMeal = () => {
   let food;
-  document.getElementById('menu').innerHTML = '';
-  if(languageBool === true){
+  document.getElementById("menu1").innerHTML = "";
+  document.getElementById("menu2").innerHTML = "";
+  if (languageBool === true) {
     food = coursesFi[Math.floor(Math.random() * coursesFi.length)];
-    document.getElementById('menu').innerHTML = '<br><p>RANDOM LOUNAS JUURI SINULLE!</p><p> * * * </p>'
-  + `<p> ${food} </p><p> * * * </p>`;
+    document.getElementById("menu1").innerHTML =
+      "<br><p>RANDOM LOUNAS JUURI SINULLE!</p><p> * * * </p>" +
+      `<p> ${food} </p><p> * * * </p>`;
+    food = coursesAll[Math.floor(Math.random() * coursesAll.length)];
+    document.getElementById("menu2").innerHTML =
+      "<p></p><p>RANDOM LOUNAS JUURI SINULLE!</p>" +
+      `<h3>${food.category}</h3>
+  <p>${food.title_fi}</p>
+  <p>${food.properties}</p>
+  <p>${food.price}</p>
+  <p></p>
+  <p> * * * </p>
+  <p></p>`;
   }
-  if(languageBool === false){
+  if (languageBool === false) {
     food = coursesEn[Math.floor(Math.random() * coursesEn.length)];
-    document.getElementById('menu').innerHTML = '<br><p>RANDOM LUNCH JUST FOR YOO!</p><p> * * * </p>'
-  + `<p> ${food} </p><p> * * * </p>`;
+    document.getElementById("menu1").innerHTML =
+      "<br><p>RANDOM LUNCH JUST FOR YOO!</p><p> * * * </p>" +
+      `<p> ${food} </p><p> * * * </p>`;
+    food = coursesAll[Math.floor(Math.random() * coursesAll.length)];
+    document.getElementById("menu2").innerHTML =
+      "<br><p>RANDOM LOUNAS JUURI SINULLE!</p>" +
+      `<h3>${food.category}</h3>
+  <p>${food.title_en}</p>
+  <p>${food.properties}</p>
+  <p>${food.price}</p>
+  <p> * * * </p>`;
   }
 };
-
+tulostaFinMenu();
 printFinMenu();
-document.getElementById('language').addEventListener('click', printMenu);
-document.getElementById('sort').addEventListener('click', sortMenu);
-document.getElementById('random').addEventListener('click', randomMeal);
-
+document.getElementById("language").addEventListener("click", printMenu);
+document.getElementById("sort").addEventListener("click", sortMenu);
+document.getElementById("random").addEventListener("click", randomMeal);
--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     ]}),
     new HtmlWebpackPlugin({
-      title: 'Dummy Menu',
+      title: 'Dummy Lounari',
       meta: {
         viewport: 'width=device-width, initial-scale=1.0'
       },
