0235fbdc3aa1e14d86185e3decdb759e43705de8
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-01-23 15:00:45 +0200

    guessing game 2nd edition with 1001 loop in console log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-console.log('Arvaa numero!');
 
-const minRandomValue = 1; //ramdom pienin arvo
-const maxRandomValue = 20; //random maksimi arvo
+const minRandomValue = 1; //ramdom min value
+const maxRandomValue = 100; //random mmax value
+const maxNumberOfGuesses = 10; //number of max guesses
 
+/**
+ * Random number for a guess game
+ */
 let randomNumber = Math.floor(Math.random() * (maxRandomValue - minRandomValue) + minRandomValue);
 
 const guesses = document.querySelector('.guesses');
@@ -12,6 +15,7 @@ const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 
 const guessSubmit = document.querySelector('.guessSubmit');
+const computerSubmit = document.querySelector('.computerSubmit');
 const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
@@ -20,9 +24,11 @@ let resetButton;
 let startingTime = 0;
 let endingTime = 0;
 
+/**
+ * Guess results
+ */
 const checkGuess = () => {
-  console.log('lähtöaika: ', startingTime);
-  const maxNumberOfGuesses = 10; //arvauksien maksimimäärä
+
   const userGuess = Number(guessField.value);
   if (guessCount === 1) {
     guesses.textContent = 'Previous guesses: ';
@@ -34,15 +40,13 @@ const checkGuess = () => {
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
     endingTime = Date.now();
-    console.log('loppuaika: ', endingTime);
-    timerTime(endingTime - startingTime); //lähetetään millisekunnit funktioon
+    timerTime(endingTime - startingTime); //sending millsecs to function
     setGameOver();
   } else if (guessCount === maxNumberOfGuesses) {
     lastResult.textContent = '!!!GAME OVER!!!';
     lowOrHi.textContent = '';
     endingTime = Date.now();
-    console.log('loppuaika: ', endingTime);
-    timerTime(endingTime - startingTime); //lähetetään millisekunnit funktioon
+    timerTime(endingTime - startingTime); //sending millsecs to function
     setGameOver();
   } else {
     lastResult.textContent = 'Wrong!';
@@ -53,31 +57,142 @@ const checkGuess = () => {
       lowOrHi.textContent = 'Last guess was too high!';
     }
   }
-  counter.textContent = 'Number of guesses: ' + guessCount; //tulostetaan arvauksien määrä
+  counter.textContent = 'Number of guesses: ' + guessCount; //printing count of guesses
   guessCount++;
   guessField.value = '';
   guessField.focus();
 };
 
-guessSubmit.addEventListener('click', checkGuess);
+let testLoopOn = true;
+let guessedNumbers = [];
+/**
+ * Computer turn
+ * If computer's quess is lower than random then min value is that number
+ * If computer's quess is higher than random then max value is that number
+ * @param {int} computerGuess
+ */
+const checkComputerGuess = (computerGuess) => {
+  guessedNumbers.push(computerGuess);
+  const maxNumberOfGuesses = 10; //number of max guesses
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+    startingTime = Date.now();
+  }
+  guesses.textContent += computerGuess + ' ';
+  if (computerGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    endingTime = Date.now();
+    timerTime(endingTime - startingTime); //sending millsecs to function
+    testLoopOn = false;
+    console.log('Computer quesses array: ', guessedNumbers);
+    console.log('Number of quesses: ', guessedNumbers.length);
+    setGameOver();
+  } else if (guessCount === maxNumberOfGuesses) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    endingTime = Date.now();
+    timerTime(endingTime - startingTime); //sending millsecs to function
+    testLoopOn = false;
+    console.log('Computer quesses array', guessedNumbers);
+    console.log('Number of quesses: ', guessedNumbers.length);
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(computerGuess < randomNumber) {
+      computerMinValue = computerGuess;
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(computerGuess > randomNumber) {
+      computerMaxValue = computerGuess;
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+  counter.textContent = 'Number of guesses: ' + guessCount; //printing count of guesses
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+};
 
+let computerMinValue = minRandomValue;
+let computerMaxValue = maxRandomValue;
+
+/**
+ * Computer's guess
+ * Best way guess a number is halving a range
+ * If min value is 1 and max value is 100 then first guess is 50 and so on
+ */
+const computerGuess = () => {
+  let computersRandomGuess = Math.floor(((computerMaxValue - computerMinValue)/2) + computerMinValue);
+  //console.log('computers number', computersRandomGuess);
+  checkComputerGuess(computersRandomGuess);
+};
+
+/**
+ * Computer game loop
+ * 1 game
+ */
+const testingLoop = () => {
+  for(let i = 0; i < maxNumberOfGuesses; i++) {
+    if(testLoopOn == false) { break; }
+    computerGuess();
+  }
+};
+
+/**
+* 1001 times test loop
+* Max guesses seems to be 10 and min 1
+* Average guesses are 5.8
+*/
+const longLoop = () => {
+  let guessedCount = [];
+  let totalCounting = 0;
+  for(let i = 0; i < 1001; i++){
+    testingLoop();
+    const guessCounter = guessCount-1;
+    guessedCount.push(guessCounter);
+    //console.log(guessCounter);
+    totalCounting += guessCounter;
+
+    resetGame();
+  }
+  const averageGuesses = totalCounting / 1001;
+  console.log('Average guesses: ', averageGuesses);
+  console.log('Max number of counts: ', Math.max(...guessedCount));
+  console.log('Min number of counts: ', Math.min(...guessedCount));
+  guessedCount = [];
+};
+
+/**
+ * Setting fields when game is over
+ */
 const setGameOver = () => {
   guessField.disabled = true;
   guessSubmit.disabled = true;
+  computerSubmit.disabled = true;
   resetButton = document.createElement('button');
   resetButton.textContent = 'Start New Game';
   document.body.append(resetButton);
   resetButton.addEventListener('click', resetGame);
 };
-//laske timeri ja tulosta se
+
+/**
+ * Counting time used on a game
+ * @param {Time} timeValue current time value
+ */
 const timerTime = (timeValue) => {
   const dateValue = new Date(timeValue);
   timeUsed.textContent = 'Time used: ' + dateValue.toLocaleString("en-US", {second: "numeric"}) + ' seconds';
 };
 
+/**
+ * Resetting a game
+ */
 const resetGame = () => {
   guessCount = 1;
-
+  guessedNumbers = [];
+  //console.log('game resetted');
   const resetParas = document.querySelectorAll('.resultParas p');
   for (const resetPara of resetParas) {
     resetPara.textContent = '';
@@ -87,10 +202,18 @@ const resetGame = () => {
 
   guessField.disabled = false;
   guessSubmit.disabled = false;
+  computerSubmit.disabled = false;
   guessField.value = '';
   guessField.focus();
+  testLoopOn = true;
 
   lastResult.style.backgroundColor = 'white';
 
   randomNumber = Math.floor(Math.random() * 100) + 1;
+  computerMinValue = minRandomValue;
+  computerMaxValue = maxRandomValue;
 };
+
+longLoop();
+guessSubmit.addEventListener('click', checkGuess);
+computerSubmit.addEventListener('click', testingLoop);
