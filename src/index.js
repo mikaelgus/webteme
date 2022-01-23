70dd7354ee589bed307e86b7bdd5dc8407a553c4
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-01-23 20:05:47 +0200

    all tasks done

--- src/index.js
+++ src/index.js
@@ -1,87 +1,71 @@
 
 const menu = document.querySelector('.menu');
 
-let coursesEn = ["Hamburger, cream sauce and poiled potates",
-                "Goan style fish curry and whole grain rice",
-                "Vegan Chili sin carne and whole grain rice",
-                "Broccoli puree soup, side salad with two napas",
-                "Lunch baguette with BBQ-turkey filling",
-                "Cheese / Chicken / Vege / Halloum burger and french fries"];
-let coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-                "Goalaista kalacurrya ja täysjyväriisiä",
-                "Vegaani Chili sin carne ja täysjyväriisi",
-                "Parsakeittoa, lisäkesalaatti kahdella napaksella",
-                "Lunch baguette with BBQ-turkey filling",
-                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
-//tulostetaan suomenkielinen menu
-const printFinMenu = () => {
-    document.getElementById('menu').innerHTML = '';
-    coursesFi.forEach(meal => {
-      document.getElementById('menu').innerHTML +=
-      `<p>${meal}</p><p> * * * </p>`;
-    });
-};
-//tulostetaan englanninkilinen menu
-const printEngMenu = () => {
-  document.getElementById('menu').innerHTML = '';
-  coursesEn.forEach(meal => {
-    document.getElementById('menu').innerHTML +=
-    `<p>${meal}</p><p> * * * </p>`;
+let courses = [
+  {name: 'Lingonberry jam', price: 4.00},
+  {name: 'Mushroom and bean casserole', price: 5.50},
+  {name: 'Chili-flavoured wheat', price: 3.00},
+  {name: 'Vegetarian soup', price: 4.80},
+  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
+];
+
+/**
+ * Validating courses names
+ */
+const validateMeal = () => {
+  courses.forEach((meal) => {
+    let name = meal.name;
+    console.log('Validating: ', name, ' == ', (/^[A-ZÖÄÅ]{1}[a-zöäå0-9 -/(),.;:]{4,64}$/.test(name)));
   });
 };
-//kielen valinta
-let languageBool = true;
+validateMeal();
+
+/**
+ * Sorting menu by price
+ */
+let sortByPrice = courses.sort((a, b) => {
+  return a.price - b.price;
+});
+console.log(sortByPrice);
+
+/**
+ * Print menu under 5€ to console
+ */
+const filterMenu = () => {
+  courses.forEach((meal) => {
+    if(meal.price < 5){
+      console.log(meal.name, ' ', meal.price);
+    }
+  });
+};
+filterMenu();
+
+const raisePrice = () => {
+  const newPrices = courses.map(meal => meal.price * 1.15);
+  console.log('uudet hinnat', newPrices);
+};
+raisePrice();
+
+const redusePrices = () => {
+  const prices = courses.map(meal => meal.price);
+  const reduseSum = prices.reduce((acc, current) => acc + current);
+  console.log('Total sum: ', reduseSum);
+};
+redusePrices();
+
+/**
+ * Printing menu
+ */
 const printMenu = () => {
-  languageBool = languageBool ? false : true;
-  if(languageBool === false){
-    printEngMenu();
-  }
-  if(languageBool === true){
-    printFinMenu();
-  }
-};
-//menun sorttaus
-let sortBool = true;
-const sortMenu = () => {
   document.getElementById('menu').innerHTML = '';
-  sortBool = sortBool ? false : true;
-  if(sortBool === false){
-    coursesEn.sort();
-    coursesFi.sort();
-    if(languageBool === true){
-      printFinMenu();
-    }
-    if(languageBool === false){
-      printEngMenu();
-    }
-  }
-  if(sortBool === true){
-    coursesEn.reverse();
-    coursesFi.reverse();
-    if(languageBool === true){
-      printFinMenu();
-    }
-    if(languageBool === false){
-      printEngMenu();
-    }
-  }
-};
-//random sapuska
-const randomMeal = () => {
-  let food;
-  document.getElementById('menu').innerHTML = '';
-  if(languageBool === true){
-    food = coursesFi[Math.floor(Math.random() * coursesFi.length)];
-  }
-  if(languageBool === false){
-    food = coursesEn[Math.floor(Math.random() * coursesEn.length)];
-  }
-  document.getElementById('menu').innerHTML = '<br><p>RANDOM LOUNAS JUURI SINULLE!</p><p> * * * </p>'
-  + `<p> ${food} </p><p> * * * </p>`;
+  courses.forEach((meal) => {
+    document.getElementById('menu').innerHTML +=
+    `<p>${meal.name}</p>
+    <p>${meal.price} €</p>
+    <p> * * * </p>`;
+  });
 };
 
-printFinMenu();
-document.getElementById('language').addEventListener('click', printMenu);
-document.getElementById('sort').addEventListener('click', sortMenu);
-document.getElementById('random').addEventListener('click', randomMeal);
+printMenu();
+
 
